036e8a0ed21edec9b528bce40b9c99e37a48e99f
BrenoCSenna <brenosenna90@yahoo.com.br>
2023-07-11 03:22:10 -0300

    Exercicio8

---
+++ Ex8.js
@@ -0,0 +1,8 @@
+let raio = 1;
+
+let comprimento = 2 * (Math.PI) * raio;
+let area = Math.PI * (raio) ** 2;
+
+console.log("Comprimento é igual a " + comprimento.toFixed(1));
+console.log("Área é igual a " + area.toFixed(2));
+
